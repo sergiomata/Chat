257c1f1ada519d05d83f3141bd78cc476ad836e1
sergiomata <32809578+sergiomata@users.noreply.github.com>
2019-07-30 00:25:52 -0500

    defining components

---
+++ src/components/MessageList.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const DUMMY_DATA = [
+    {
+        senderId: 'perborgen',
+        text: 'Hey, how is it going?'
+    },
+    {
+        senderId: 'janedoe',
+        text: 'Great! How about you?'
+    },
+    {
+        senderId: 'perborgen',
+        text: 'Good to hear! I am great as well'
+    }
+]
+
+class MessageList extends  React.Component {
+    render() {
+        return (
+            <div className="message-list">
+                {DUMMY_DATA.map((message, index) => {
+                    return (
+                        <div>{message.text}</div>
+                    )
+                })}
+            </div>
+        );
+    }
+}
+
+export default MessageList;
