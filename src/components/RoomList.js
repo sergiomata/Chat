dd05715b08d7f8fab92846b1bb854caa2785b658
Sergio Mata <serchmata@gmail.com>
2019-08-11 21:13:29 -0500

    fix: fix random order rooms changing  joinable to joinable  rooms

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -2,18 +2,18 @@ import React from 'react';
 
 class RoomList extends  React.Component {
     render() {
-        console.log(this.props.rooms);
+        const orderedRooms = [...this.props.rooms].sort((a,b) => a.id - b.id)
         return (
             <div className="rooms-list">
                 <ul>
                     <h3>Your rooms:</h3>
-                    {this.props.rooms.map( room => {
+                    {orderedRooms.map( room => {
                         return(
                             <li key={room.id} className="room">
                                 <a 
                                     onClick={() => this.props.subscribeToRoom(room.id)} 
                                     href="#">#{room.name}
-                                </a>
+                                    </a>
                             </li>
                         )
                     })}
